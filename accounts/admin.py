dcf9052ce8e3d037470af4940786cc07d1b5d829
= <=>
2023-06-16 14:22:23 +0200

    Customize profile admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+from .utils import create_profile_html
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'create_at', 'update_at']
+    search_fields = ['user__first_name', 'user__last_name', 'user__nick_name', 'position', 'bio']
+    date_hierarchy = 'create_at'
+    readonly_fields = ['user', 'create_at', 'update_at', 'show_image', 'show_cover']
+    fieldsets = (
+        ('User Main Info', {'fields': (
+            'user',
+            'position',
+            'bio',
+            ('phone_number_1', 'phone_number_2'),
+            'qr_code'
+        )}),
+        ('Address', {'fields': (
+            ('city', 'country'),
+            'address',
+        )}),
+        ('Images', {'fields': (
+            ('image', 'show_image'),
+            ('cover', 'show_cover'),
+        )}),
+    )
+
+    def show_image(self, obj):
+        if obj.image:
+            return create_profile_html(obj.image)
+        return ''
+    show_image.short_description = ''
+
+    def show_cover(self, obj):
+        if obj.cover:
+            return create_profile_html(obj.cover)
+        return ''
+    show_cover.short_description = ''
+
+
+admin.site.register(Profile, ProfileAdmin)
--- accounts/utils.py
+++ accounts/utils.py
@@ -1,5 +1,6 @@
 import random
 from typing import List
+from django.utils.safestring import mark_safe
 
 
 def generate_random_number(lower_limit: int, upper_limit: int, exclude: List[int] = None) -> int:
@@ -11,8 +12,12 @@ def generate_random_number(lower_limit: int, upper_limit: int, exclude: List[int
     return number
 
 
-def get_object_or_none(model,  *args, **kwargs):
+def get_object_or_none(model, *args, **kwargs):
     try:
         return model.objects.get(*args, **kwargs)
     except model.DoesNotExist:
         return None
+
+
+def create_profile_html(image):
+    return mark_safe(f"""<a href='{image.url}'><img src="{image.url}" style="height:60%; border-radius: 50%; border: 6px solid gray;"></a>""")
