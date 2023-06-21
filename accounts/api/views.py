042ee551ea0ebd8e5d03d1c2cc67133e094747bb
= <=>
2023-06-22 00:06:02 +0200

    Add actions to visit log view

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,5 +1,8 @@
+from rest_framework.decorators import action
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, ListModelMixin
+
+from drf_spectacular.utils import extend_schema
 
 from accounts.models import Profile, VisitLog
 from .permissions import IsUserWithProfile
@@ -25,3 +28,23 @@ class VisitLogViewSet(AllowAnyInSafeMethodOrCustomPermissionMixin, CreateModelMi
 
     def perform_create(self, serializer):
         serializer.save(visitor=self.request.user)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.action == 'my_visits':
+            return queryset.filter(visitor=self.request.user)
+        elif self.action == 'my_views':
+            return queryset.filter(profile=self.request.user.profile)
+        return queryset
+
+    @extend_schema(responses={200: VisitLogSerializer(many=True)})
+    @action(detail=False, methods=['GET'], name='Get My Visits', url_path='my-visits')
+    def my_visits(self, request, *args, **kwargs):
+        """Get list of visits that user made"""
+        return self.list(request, *args, **kwargs)
+
+    @extend_schema(responses={200: VisitLogSerializer(many=True)})
+    @action(detail=False, methods=['GET'], name='Get My Views', url_path='my-views')
+    def my_views(self, request,  *args, **kwargs):
+        """Get list of user profile visits that others have made"""
+        return self.list(request, *args, **kwargs)
