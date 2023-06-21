5fddc65b14bb1a9544a6c5262d775c3db54e1d54
= <=>
2023-06-21 22:13:15 +0200

    Modify object permission

--- accounts/api/permissions.py
+++ accounts/api/permissions.py
@@ -1,16 +1,22 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
+from account.models import Profile, VisitLog
+
 
 class ReadOnly(BasePermission):
 
-    def has_permission(self, request, view):
+    def has_permission(self, request, view) -> bool:
         return request.method in SAFE_METHODS
 
 
 class IsUserWithProfile(BasePermission):
 
-    def has_permission(self, request, view):
+    def has_permission(self, request, view) -> bool:
         return request.user.is_authenticated and hasattr(request.user, 'profile')
 
-    def has_object_permission(self, request, view, obj):
-        return request.user == obj.user
+    def has_object_permission(self, request, view, obj) -> bool:
+        if isinstance(obj, Profile):
+            return request.user == obj.user
+        if isinstance(obj, VisitLog):
+            return request.user == obj.visitor
+        return False
