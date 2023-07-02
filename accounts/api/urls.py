0cbea323d58ccc3f5d01c3d5e2284f9561fb5d67
= <=>
2023-07-03 00:09:14 +0200

    Customize djoser user view set

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, re_path, include
 from rest_framework import routers
 from djoser.social.views import ProviderAuthView
 
-from .views import ProfileViewSet, VisitLogViewSet
+from .views import ProfileViewSet, VisitLogViewSet, UserViewSet
 
 
 app_name = 'accounts'
@@ -11,9 +11,9 @@ app_name = 'accounts'
 router = routers.DefaultRouter()
 router.register(r'profile', ProfileViewSet, basename='profile')
 router.register(r'visit', VisitLogViewSet, basename='visit')
+router.register(r'auth/users', UserViewSet, basename='user')
 
 urlpatterns = [
-    path('auth/', include('djoser.urls.base'), name='base'),
     path('auth/', include('djoser.urls.jwt'), name='jwt'),
     re_path(r"^auth/social/(?P<provider>\S+)/$", ProviderAuthView.as_view(), name="social-auth-provider"),
     path('', include(router.urls), name='routes'),
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -4,13 +4,14 @@ from rest_framework.decorators import action
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, ListModelMixin
 
-from drf_spectacular.utils import extend_schema
+from djoser.views import UserViewSet as DjoserUserViewSet
+from drf_spectacular.utils import extend_schema, OpenApiResponse
 
-from accounts.models import Profile, VisitLog
+from accounts.models import User, Profile, VisitLog
 from .permissions import IsUserWithProfile
-from .filters import ProfileFilter, VisitLogFilter
+from .filters import ProfileFilter, VisitLogFilter, UserFilter
 from .serializers import ProfileSerializer, VisitLogSerializer
-from .mixins import AllowAnyInSafeMethodOrCustomPermissionMixin
+from .mixins import AllowAnyInSafeMethodOrCustomPermissionMixin, DestroyMethodNotAllowedMixin
 
 
 class ProfileViewSet(AllowAnyInSafeMethodOrCustomPermissionMixin, RetrieveModelMixin, UpdateModelMixin, ListModelMixin,
@@ -75,3 +76,24 @@ class VisitLogViewSet(AllowAnyInSafeMethodOrCustomPermissionMixin, CreateModelMi
     def my_views(self, request,  *args, **kwargs):
         """Get list of user profile visits that others have made"""
         return self.list(request, *args, **kwargs)
+
+
+class UserViewSet(DestroyMethodNotAllowedMixin, DjoserUserViewSet):
+    queryset = User.objects.with_profile()
+    filterset_class = UserFilter
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = super(UserViewSet, self).get_queryset()
+        if self.action == 'list' and hasattr(user, 'profile'):
+            queryset.exclude(user)
+        return queryset
+
+    @extend_schema(responses={status.HTTP_405_METHOD_NOT_ALLOWED:
+                              OpenApiResponse(description='Delete user is not allowed')})
+    def destroy(self, request, *args, **kwargs):
+        return super(UserViewSet, self).destroy(request, *args, **kwargs)
+
+    @action(["get", "put", "patch"], detail=False)
+    def me(self, request, *args, **kwargs):
+        return super().me(request, *args, **kwargs)
